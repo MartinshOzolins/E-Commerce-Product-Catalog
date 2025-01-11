e5f3064319aedad9b1156242d90b786c95ee3f2a
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-01-11 14:46:48 +0200

    Update cart features: allow quantity changes and display item count

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,35 +1,114 @@
-//context
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../contexts/context";
 import { NavLink } from "react-router-dom";
 
+// MUI icons
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
 export default function Cart() {
-  const { cart } = useContext(GlobalContext);
-  // const filterArray = () => {
-  //   if (cart.length === 0 || cart.length === 1) return;
-  //   let filteredArray = [];
-  //   cart.forEach((product) => {});
-  // };
-  // const filteredArray = filterArray() || [];
-  const filteredArray = [];
+  const { cart, setCart } = useContext(GlobalContext);
+  const [filteredArray, setFilteredArray] = useState([]);
+
+  // Merge identical items in the cart (runs whenever the cart is updated)
+  const mergeCart = (cart) => {
+    const mergedArray = [];
+    cart.forEach((product) => {
+      // Cheks whether items already exists in mergedArray
+      const existingProduct = mergedArray.find(
+        (item) => item.id === product.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        mergedArray.push({ ...product, quantity: 1 });
+      }
+    });
+    return mergedArray;
+  };
+
+  // Updates filteredArray whenever the cart changes
+  useEffect(() => {
+    setFilteredArray(mergeCart(cart));
+  }, [cart]);
+
+  const handleIncrement = (productToIncrement) => {
+    setCart(() => {
+      let newArray = [];
+      // Loops through filteredArray
+      filteredArray.forEach((product) => {
+        // Finds product to increment
+        if (product.id === productToIncrement.id) {
+          // Retrieves count of repeated products
+          const count = productToIncrement.quantity + 1;
+          for (let i = 0; i < count; i++) {
+            //loops count times and adds product as many times as is updated count
+            // allowing to merge them again when page re-loads
+            newArray.push(product);
+          }
+        } else {
+          const count = product.quantity;
+          const updatedProduct = { ...product };
+          for (let i = 0; i < count; i++) {
+            newArray.push(updatedProduct);
+          }
+        }
+      });
+      return newArray;
+    });
+  };
+
+  const handleDecrement = (productToDecrement) => {
+    setCart(() => {
+      let newArray = [];
+      // Loops through filteredArray
+      filteredArray.forEach((product) => {
+        // Finds product to decrement and checks whether quantity > 1
+        if (product.id === productToDecrement.id && product.quantity > 1) {
+          // decrements quantity and returns updated quantity
+          const count = productToDecrement.quantity - 1;
+          for (let i = 0; i < count; i++) {
+            //loops count times and adds product to newArray as many times as is updated count
+            // allowing to merge them again when page re-loads
+            newArray.push(product);
+          }
+        } else {
+          const count = product.quantity;
+          const updatedProduct = { ...product };
+          for (let i = 0; i < count; i++) {
+            //loops count times and adds product as many times as is updated count
+            // allowing to merge them again when page re-loads
+            newArray.push(updatedProduct);
+          }
+        }
+      });
+      return newArray;
+    });
+  };
 
   return (
     <div className="w-full flex flex-col justify-center items-center px-4">
       <h1 className="text-2xl font-bold ">Your Cart</h1>
-      <h2 className="text-lg mb-4">Items: {cart.length}</h2>
+      <h2 className="text-lg mb-4">
+        Items:{" "}
+        {filteredArray.reduce(
+          (numOfItems, product) => numOfItems + product.quantity,
+          0
+        )}
+      </h2>
       <div
-        className={
-          `w-full flex justify-center md:justify-start` + cart.length === 0
+        className={`w-full flex ${
+          filteredArray.length === 0
             ? "justify-center"
-            : null
-        }
+            : "justify-start md:justify-start"
+        }`}
       >
         <NavLink to="/" className="bg-gray-500 text-white px-4 py-2 rounded">
           Continue Shopping
         </NavLink>
       </div>
       {filteredArray.length === 0 ? (
-        <div className="flex flx-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
           <p className="text-lg my-5">Your cart is empty</p>
         </div>
       ) : (
@@ -46,22 +125,40 @@ export default function Cart() {
                     className="w-full h-full object-cover"
                     src={
                       product.images?.[0]?.includes("any")
-                        ? "../public/default.webp"
+                        ? "/default.webp"
                         : product.images?.[0]?.replace(/[["\]]/g, "") ||
-                          "../public/default.webp"
+                          "/default.webp"
                     }
                     alt={product.title}
                   />
                 </div>
                 <div className="h-full flex flex-col ml-4 text-sm items-start justify-start">
-                  <p className="font-medium">{product.title}</p>
-                  <p className="text-gray-500 text-xs">${product.price}</p>
-                  <div className="place-self-end flex flex-col">
-                    <p>
-                      Quantity:
-                      <input value={1} className="ml-1 w-5" />
-                    </p>
-                    <button>Remove</button>
+                  <div className="flex flex-col">
+                    <p className="font-medium">{product.title}</p>
+                    <p className="text-gray-500 text-xs">${product.price}</p>
+                  </div>
+                  <div className="flex flex-row items-center ">
+                    <div className="flex flex-row items-center">
+                      <span>Quantity:</span>
+                      <input
+                        value={product.quantity}
+                        className="ml-1 w-12 text-center border rounded"
+                        readOnly
+                      />
+                      <div className="flex flex-col ml-1">
+                        <AddIcon
+                          style={{ fontSize: 15, cursor: "pointer" }}
+                          onClick={() => handleIncrement(product)}
+                        />
+                        <RemoveIcon
+                          style={{ fontSize: 15, cursor: "pointer" }}
+                          onClick={() => handleDecrement(product)}
+                        />
+                      </div>
+                    </div>
+                    <button className="text-red-500 hover:underline ml-2 mb-1">
+                      Remove
+                    </button>
                   </div>
                 </div>
               </div>
@@ -72,8 +169,11 @@ export default function Cart() {
             <p className="mb-2">
               Subtotal:{" "}
               <span className="font-medium">
-                {cart
-                  .reduce((sum, product) => sum + product.price, 0)
+                {filteredArray
+                  .reduce(
+                    (sum, product) => sum + product.price * product.quantity,
+                    0
+                  )
                   .toFixed(2)}
                 $
               </span>
@@ -83,7 +183,10 @@ export default function Cart() {
               Estimated Total:{" "}
               <span className="font-bold">
                 {(
-                  cart.reduce((sum, product) => sum + product.price, 0) + 2
+                  filteredArray.reduce(
+                    (sum, product) => sum + product.price * product.quantity,
+                    0
+                  ) + 2
                 ).toFixed(2)}
                 $
               </span>
